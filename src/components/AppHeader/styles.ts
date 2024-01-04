5ebd4400651b2372a3c005374cdd3b4710d0853c
EliasYoc <eliyoc_97@hotmail.com>
2024-01-03 21:05:09 -0600

    fix: sticky header whole vh

--- src/components/AppHeader/styles.ts
+++ src/components/AppHeader/styles.ts
@@ -4,11 +4,13 @@ import { mediaQueries } from "../../utils/helper";
 export const Header = styled.header`
   padding: 1rem;
   box-shadow: 0 0 0.5rem 0 rgba(0, 0, 0, 0.2);
-  position: sticky ;
+  position: absolute ;
+  width:100% ;
   top: 0;
   height: var(--header-height) ;
-  background: #ffffff4d ;
-  backdrop-filter: blur(15px);
+  background: #ffffff6e ;
+  backdrop-filter: blur(17px);
+  view-transition-name: app-header;
   z-index: 2 ;
   @media only screen and (${mediaQueries.md}) {
     top: auto;
--- src/globalStyledComponents.ts
+++ src/globalStyledComponents.ts
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const ViewFullHeight = styled.div`
   flex-grow: 1;
+  padding-bottom: var(--header-height);
   overflow: auto;
 `
