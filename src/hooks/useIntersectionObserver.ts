f6464233702fb0ee75bfa5d5aa83d2bbd7eb4e9d
EliasYoc <eliyoc_97@hotmail.com>
2024-02-10 17:37:30 -0600

    fix: hook Iobserver

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,22 +1,33 @@
 import { useEffect } from "react";
 
+interface IntersectionObserverProps {
+  onIntersect: IntersectionObserverCallback;
+  provideElementsToObserve: () =>
+    | HTMLElement[]
+    | NodeListOf<HTMLElement>
+    | HTMLCollectionOf<HTMLElement>;
+  provideOptions: () => IntersectionObserverInit;
+}
 const useIntersectionObserver = ({
   provideElementsToObserve,
   onIntersect,
   provideOptions,
-}) => {
+}: IntersectionObserverProps) => {
   useEffect(() => {
-    const elementOrElements = provideElementsToObserve();
-    const options = provideOptions();
-    const observer = new IntersectionObserver(onIntersect, options);
-
-    if (Array.isArray(Array.from(elementOrElements))) {
-      elementOrElements.forEach((element) => {
+    const providedElements = provideElementsToObserve();
+    
+    if (
+      !(Array.isArray(providedElements) ||
+      providedElements instanceof NodeList ||
+      providedElements instanceof HTMLCollection)
+    ) throw new Error("provideElementsToObserve must return an array of elements or NodeList or HTMLCollection");
+    
+      const elements = Array.from(providedElements)
+      const options = provideOptions();
+      const observer = new IntersectionObserver(onIntersect, options);
+      elements.forEach((element) => {
         observer.observe(element);
       });
-    } else {
-      observer.observe(elementOrElements);
-    }
 
     return () => {
       observer.disconnect();
